e6578c9794683fa7e4773a1eae772ab361dc9afc
Maria Cholakova <mimi_0408@abv.bg>
2019-01-11 11:59:08 +0200

    Fixing static_content/Dockerfile and db_server

--- db_server/create_database.sql
+++ db_server/create_database.sql
@@ -5,73 +5,50 @@ CREATE USER 'fashiondbuser'@'%' IDENTIFIED BY 'fashiondbpass';
 GRANT ALL PRIVILEGES ON fashiondb.* to 'fashiondbuser'@'%';
 FLUSH PRIVILEGES;
 
-DROP TABLE IF EXISTS `products`;
-DROP TABLE IF EXISTS `customers`;
-DROP TABLE IF EXISTS `procucts_in_carts`;
-DROP TABLE IF EXISTS `procuct_names`;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS procucts_in_carts;
+DROP TABLE IF EXISTS procuct_names;
 
-CREATE TABLE `products` (
-  `product_id` INT NOT NULL AUTO_INCREMENT,
-  `product_price` FLOAT(5,2) NOT NULL,
-  `product_name` VARCHAR(40) NOT NULL,
-  `count_available` INT NOT NULL DEFAULT 30,
-  --`product_size` ENUM('x-small', 'small', 'medium', 'large', 'x-large'),
-  `product_sex` ENUM('men', 'women'),
-  `description` VARCHAR(100) NOT NULL
-   PRIMARY KEY(`product_id`)
-) sENGINE=InnoDB;
-
-CREATE TABLE `customers` (
-  `customer_id` INT NOT NULL AUTO_INCREMENT,
-  `customer_name` VARCHAR(40) NOT NULL,
-  `cutomer_password` VARCHAR(20) NOT NULL,
-  `address` VARCHAR(40) NOT NULL,
-  `town` VARCHAR(40) NOT NULL,
-  PRIMARY KEY(`cutomer_id`)
+CREATE TABLE products (
+   product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   product_price  FLOAT(5,2) NOT NULL,
+   product_name  VARCHAR(40) NOT NULL,
+   count_available  INT NOT NULL DEFAULT 30,
+   product_sex  ENUM('men', 'women'),
+   description  VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE `product_in_carts` (
-  `order_id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `date_ordered` TIMESTAMP DEFAULT NULL,  --if the item has not been ordered, this field is null 
-  PRIMARY KEY(`order_id`),
-  FOREIGN KEY(`customer_id`) REFERENCES `customers`(`customer_id`),
-  FOREIGN KEY(`product_id`) REFERENCES `products`(`product_id`)
+CREATE TABLE  customers  (
+   customer_id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   customer_name  VARCHAR(40) NOT NULL,
+   customer_password  VARCHAR(20) NOT NULL,
+   address  VARCHAR(40) NOT NULL,
+   town  VARCHAR(40) NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE `product_names` (
-   `productname_id` INT NOT NULL AUTO_INCREMENT,
-   `name` VARCHAR(40) NOT NULL
+INSERT INTO customers (customer_name, customer_password, address, town) VALUES ("maria", "Asdf1", "Luben Karavelov 10", "Rogosh");
+
+CREATE TABLE  products_in_carts  (
+   order_id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   customer_id  INT NOT NULL,
+   product_id  INT NOT NULL,
+   quantity  INT NOT NULL,
+   date_ordered  TIMESTAMP DEFAULT NULL,
+   FOREIGN KEY (customer_id) REFERENCES  customers(customer_id),
+   FOREIGN KEY (product_id) REFERENCES  products(product_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO `product_names` (`name`) VALUES 
-("jeans"), 
-("shirt"),
-("jacket");
 
-
-DELIMITER $
-DROP PROCEDURE IF EXISTS `populate_products` $
-CREATE PROCEDURE populate_products(entries INT, sex NVARCHAR(5))
-    BEGIN
-        DECLARE pr INT DEFAULT 1;
-        WHILE  pr <= 3 
-            DO           
-                DECLARE i INT DEFAULT 1;
-                WHILE i <= entries
-                    DO INSERT INTO `products` (`product_price`, `product_name`, `product_sex`, `product_description`) VALUES
-                        (   (ROUND(RAND() * 0.200 + 0.01, 2)), 
-                            (SELECT name FROM product_names where productname_id = pr),
-                            sex,
-                            (CONCAT ("Our product ", (SELECT name FROM product_names where productname_id = pr), "-", i, " is very comfortable and looks great!"))
-                        ); 
-                    SET i = i + 1;
-                END WHILE;
-        END WHILE;
-    END $
-DELIMITER ;
-
-EXEC populate_products(2, "men");
-EXEC populate_products(2, "women");
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jeans", "men", "dolce & gabbana jeans!" );
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jeans", "men", "Louis Vuitton jeans!" );           
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jeans", "women", "The dolce & gabbana jeans are great!" );                          
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jeans", "women", "The Louis Vuitton are great!" );                     
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "shirt", "men", "This D&G shirt is great!" );
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "shirt", "men", "This LV shirt is great!" );
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "shirt", "women", "This ladies D&G shirt is great!" );           
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "shirt", "women", "This ladies LV shirt is great!" );                          
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jacket", "men", "dolce & gabbana men jacket!" );                     
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jacket", "men", "Louis Vuitton men jacket" );
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jacket", "women", "dolce & gabbana women jacket!" );
+INSERT INTO products (product_price, product_name, product_sex,  description) VALUES ( ROUND(RAND() *  200 +  1, 2), "jacket", "women", "Louis Vuitton women jacket!" );           
